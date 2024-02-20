1675c3058d188bd9dcfc360a79adf08dfcbfbe83
Jonne Tanninen <jonne.tanninen@smartly.io>
2024-02-20 12:35:41 +0200

    Add logic for setting user position and rendering marker

--- src/modules/locator/components/LocatorMap.tsx
+++ src/modules/locator/components/LocatorMap.tsx
@@ -3,14 +3,27 @@
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet-defaulticon-compatibility';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, useMapEvents } from 'react-leaflet';
 import { EntityWithLocation } from '../types';
+import { useState } from 'react';
+import { LatLng } from 'leaflet';
 
 type Props = {
   entities: EntityWithLocation[];
 };
 
+const LocationFinder = ({ setUserPosition }: { setUserPosition: (arg0: LatLng) => void }) => {
+  const map = useMapEvents({
+    click(e) {
+      setUserPosition(e.latlng);
+    },
+  });
+  return null;
+};
+
 export const LocatorMap = ({ entities }: Props) => {
+  const [userPosition, setUserPosition] = useState<LatLng>();
+
   return (
     <div className="h-200 flex-1 max-w-screen-2xl">
       <MapContainer
@@ -24,10 +37,12 @@ export const LocatorMap = ({ entities }: Props) => {
         ]}
         className="h-full"
       >
+        <LocationFinder setUserPosition={setUserPosition} />
         <TileLayer
           attribution="Google Maps"
           url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
         />
+        {userPosition && <Marker position={userPosition} />}
         {entities.map((x) => (
           <Marker
             position={[x.lat, x.long]}
