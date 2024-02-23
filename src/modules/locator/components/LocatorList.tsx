f50c145f6f9f35923251c31f223b9d6a9428f8a4
Jonne Tanninen <jonne.tanninen@smartly.io>
2024-02-23 13:36:36 +0200

    Small style tweaking and add comment

--- src/modules/locator/components/LocatorList.tsx
+++ src/modules/locator/components/LocatorList.tsx
@@ -26,16 +26,20 @@ const LocatorList = ({ entities, userPosition }: Props) => {
           key={entity.id}
           className="bg-stone-100 p-8 rounded-md flex gap-x-6 text-stone-800 shadow-md flex-wrap"
         >
-          <Image
-            alt=""
-            className="w-auto h-24"
-            // Height and width needs to be provided here for next/image to avoid layout shift with external images
-            height={96}
-            width={71}
-            src={entity.image}
-            aria-hidden
-          />
-          <div className="flex justify-between flex-1 flex-col lg:flex-row gap-3">
+          <div className="w-20">
+            <Image
+              alt=""
+              // Using tailwind to set height and width cause console warning because of known bug: https://github.com/vercel/next.js/issues/56025
+              className="w-auto h-24"
+              // Height and width needs to be provided here for next/image to avoid layout shift with external images
+              height={96}
+              width={71}
+              src={entity.image}
+              aria-hidden
+            />
+          </div>
+
+          <div className="flex justify-between flex-1 flex-col lg:flex-row gap-3 w-">
             <div className="flex flex-col">
               <h2 className="text-lg font-bold">{entity.name}</h2>
               <span className="capitalize">Species: {entity.species}</span>
@@ -49,6 +53,7 @@ const LocatorList = ({ entities, userPosition }: Props) => {
                   // Height and width needs to be provided here for next/image to avoid layout shift with external images
                   height={24}
                   width={15}
+                  // Using tailwind to set height and width cause console warning because of known bug: https://github.com/vercel/next.js/issues/56025
                   className="h-6 w-auto mr-2"
                   src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png"
                   aria-hidden
